575c2da2382ba1679e20966d1a0494bf22b9fb43
RisDicom <risto.hirvilammi@ovph.fi>
2026-04-23 12:33:39 +0300

    build: centralize version to GlobalAssemblyInfo.cs, bump to 0.9.2-beta

--- EQD2Viewer.Esapi/Properties/AssemblyInfo.cs
+++ EQD2Viewer.Esapi/Properties/AssemblyInfo.cs
@@ -1,18 +1,9 @@
-﻿using System.Reflection;
+using System.Reflection;
 using System.Runtime.InteropServices;
 
+// Version, company, product, and copyright come from GlobalAssemblyInfo.cs
 [assembly: AssemblyTitle("EQD2Viewer.Esapi")]
 [assembly: AssemblyDescription("ESAPI adapter layer for EQD2 Viewer -- Eclipse script entry point")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("EQD2Viewer.Esapi")]
-[assembly: AssemblyCopyright("")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
 
 [assembly: ComVisible(false)]
 [assembly: Guid("A1B2C3D4-E5F6-7890-ABCD-EF1234567890")]
-
-[assembly: AssemblyVersion("0.9.1.0")]
-[assembly: AssemblyFileVersion("0.9.1.0")]
-[assembly: AssemblyInformationalVersion("0.9.1-beta")]
--- EQD2Viewer.FixtureGenerator/Properties/AssemblyInfo.cs
+++ EQD2Viewer.FixtureGenerator/Properties/AssemblyInfo.cs
@@ -1,13 +1,9 @@
-﻿using System.Reflection;
+using System.Reflection;
 using System.Runtime.InteropServices;
 
+// Version, company, product, and copyright come from GlobalAssemblyInfo.cs
 [assembly: AssemblyTitle("EQD2Viewer.FixtureGenerator")]
 [assembly: AssemblyDescription("Generates JSON test fixtures from Eclipse plan data")]
-[assembly: AssemblyCompany("OVPH")]
-[assembly: AssemblyProduct("EQD2Viewer.FixtureGenerator")]
-[assembly: AssemblyCopyright("Copyright © OVPH2026")]
+
 [assembly: ComVisible(false)]
 [assembly: Guid("f1e2d3c4-b5a6-7890-cafe-babe12345678")]
-[assembly: AssemblyVersion("0.9.1.0")]
-[assembly: AssemblyFileVersion("0.9.1.0")]
-[assembly: AssemblyInformationalVersion("0.9.1-beta")]
